31f3e55d7fa410abf6e1f352e9e8cdb527027bb2
Kristofer Norman <kjn2418@email.vccs.edu>
2024-02-20 22:49:06 -0500

    Added many to many relationship models and updated the existing models

--- PokemonReviewApp/Models/Owner.cs
+++ PokemonReviewApp/Models/Owner.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; }
         public string Gym { get; set; }
         public Country Country { get; set; }
-
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
     }
 } 
--- PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -6,5 +6,8 @@
         public string Name { get; set; }
         public DateTime Birthdate { get; set; }
         public ICollection<Review> Reviews { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
+
     }
 }
