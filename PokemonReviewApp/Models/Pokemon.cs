f11faaf7af1399ad32815a14836f953afb34f57e
Kristofer Norman <kjn2418@email.vccs.edu>
2024-02-21 00:18:57 -0500

    Created Data Context, Updated the many to many relationships model, Used dependency injection to add the DbContext, Seeded the Database, Migration, Ran the database seed.

--- PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime Birthdate { get; set; }
+        public DateTime BirthDate { get; set; }
         public ICollection<Review> Reviews { get; set; }
         public ICollection<PokemonOwner> PokemonOwners { get; set; }
         public ICollection<PokemonCategory> PokemonCategories { get; set; }
--- PokemonReviewApp/Models/PokemonCategory.cs
+++ PokemonReviewApp/Models/PokemonCategory.cs
@@ -2,7 +2,7 @@
 {
     public class PokemonCategory
     {
-        public int Id { get; set; }
+        public int PokemonId { get; set; }
         public int CategoryId { get; set; }
         public Pokemon Pokemon { get; set; }
         public Category Category { get; set; }
