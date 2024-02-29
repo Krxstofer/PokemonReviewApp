f8b104ff4c9ef7fcdf10e70e8691f7e31424582c
Kristofer Norman <kjn2418@email.vccs.edu>
2024-02-28 21:43:19 -0500

    Added more members to the Interface, Added a helper class that contains Automapper. Added service to register automapper. Added pokemon dto class for filteriong and updated the pokemon repository.

--- PokemonReviewApp/Repository/PokemonRepository.cs
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -13,9 +13,34 @@ namespace PokemonReviewApp.Repository
             _context = context;
         }
 
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemons.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemons.Where(p => p.Name == name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _context.Reviews.Where(p => p.Pokemon.Id == pokeId);
+
+                if (review.Count() <= 0)
+                    return 0;
+
+                return ((decimal)review.Sum(r => r.Rating) / review.Count());
+        }
+
         public ICollection<Pokemon> GetPokemons()
         {
             return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+
+        public bool PokemonExists(int pokeid)
+        {
+            return _context.Pokemons.Any(p => p.Id == pokeid);
         }
     }
 }
