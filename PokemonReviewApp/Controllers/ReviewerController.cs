e64d9f464ae88ddd457e7edcc8dd2856aaccf19c
Kristofer Norman <kjn2418@email.vccs.edu>
2024-03-06 03:00:13 -0500

    Http post request to the api.

--- PokemonReviewApp/Controllers/ReviewerController.cs
+++ PokemonReviewApp/Controllers/ReviewerController.cs
@@ -64,5 +64,39 @@ namespace PokemonReviewApp.Controllers
 
             return Ok(reviews);
         }
+
+
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(204)]
+
+        public IActionResult CreateReview([FromBody] ReviewerDto reviewerCreate)
+        {
+            if (reviewerCreate == null)
+                return BadRequest(ModelState);
+
+            var reviewers = _reviewerRepository.GetReviewers()
+                .Where(c => c.LastName.Trim().ToUpper() == reviewerCreate.LastName.TrimEnd().ToUpper())
+                .FirstOrDefault();
+
+            if (reviewers != null)
+            {
+                ModelState.AddModelError("", "Reviewer already exists");
+                return StatusCode(422, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewerMap = _mapper.Map<Reviewer>(reviewerCreate);
+
+            if (!_reviewerRepository.CreateReviewer(reviewerMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while saving");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok("Successfully created");
+        }
     }
 }
--- PokemonReviewApp/Repository/ReviewerRepository.cs
+++ PokemonReviewApp/Repository/ReviewerRepository.cs
@@ -35,5 +35,18 @@ namespace PokemonReviewApp.Repository
         {
             return _context.Reviewers.Any(r => r.Id == reviewerId);
         }
+
+        public bool CreateReviewer(Reviewer reviewer)
+        {
+            _context.Add(reviewer);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+
+            return saved > 0 ? true : false;
+        }
     }
 }
